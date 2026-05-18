c7793d36fe52deba9c5997259f95ef492ab0d781
molochnicha2000-hue <molochnicha2000@gmail.com>
2026-05-18 20:21:58 +1000

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.py
@@ -0,0 +1,31 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        todoList = {}
+        for i in range(numCourses):
+            todoList[i] = []
+
+        for cur, need in prerequisites:
+            todoList[cur].append(need)
+
+        # return False if there is a loop
+        visit = set()
+        def dfs(cur):
+            if cur in visit:
+                return False
+
+            if not todoList[cur]:
+                return True
+            
+            visit.add(cur)
+            for i in todoList[cur]:
+                if not dfs(i):
+                    return False
+            
+            visit.remove(cur)
+            return True
+        
+        for i in todoList:
+            for j in todoList[i]:
+                if not dfs(j):
+                    return False
+        return True
