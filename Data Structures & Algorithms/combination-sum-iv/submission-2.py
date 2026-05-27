c593cef47da6e3ad49c4a8ae15eade41696cd97d
molochnicha2000-hue <molochnicha2000@gmail.com>
2026-05-27 13:51:30 +1000

    Add: combination-sum-iv - submission-2

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        count = 0
+        N = len(nums)
+        storage = []
+        dp = {}
+        def dfs(i, amount):
+            if amount == target:
+                return 1
+
+            if i >= N or amount > target:
+                return 0
+            
+            if (i, amount) in dp:
+                return dp[(i, amount)]
+
+            temp = 0
+            for j in range(N):
+                if amount + nums[j] <= target:
+                    temp += dfs(j, amount + nums[j])
+
+            dp[(i, amount)] = temp
+            return temp
+        # if set(cur) == 1 only 1 way
+        # else count += len(cur)
+        res = dfs(0, 0)
+       
+        return res
+        
