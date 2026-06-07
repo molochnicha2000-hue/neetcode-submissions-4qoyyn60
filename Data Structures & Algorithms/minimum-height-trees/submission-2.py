8377049a7a64959d4bae752dbf4305e2ba22a772
molochnicha2000-hue <molochnicha2000@gmail.com>
2026-06-07 17:28:12 +1000

    Add: minimum-height-trees - submission-2

---
+++ Data Structures & Algorithms/minimum-height-trees/submission-2.py
@@ -0,0 +1,54 @@
+from sortedcontainers import SortedSet
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        if not edges:
+            return [0]
+        adg = {i : [] for i in range(n)}
+
+        for ai, bi in edges:
+            adg[ai].append(bi)
+            adg[bi].append(ai)
+
+        def bfs(node):
+            q = collections.deque([node])
+            res = 0
+            while q:
+                for _ in range(len(q)):
+                    cur = q.popleft()
+                    visit.add(cur)
+                    for nei in adg[cur]:
+                        if nei not in visit:
+                            q.append(nei)
+                res += 1
+            return res
+
+        visit = set()
+        res = SortedSet()
+        for ai, bi in edges:
+            first = bfs(ai)
+            visit.clear()
+
+            second = bfs(bi)
+            visit.clear()
+
+            res.add((first, ai))
+            res.add((second, bi))
+
+        res = list(res)
+        N = len(res)
+        ans = collections.defaultdict(list)
+        prev = res[0][0]
+        for val, index in res:
+            if val != prev:
+                break
+            ans[val].append(index)
+            prev = val
+        
+        return ans[prev]
+            
+
+
+
+
+
+
