23ff67d027114ac832bae4027b725141eaa81581
molochnicha2000-hue <molochnicha2000@gmail.com>
2026-06-11 12:52:31 +1000

    Add: search-insert-position - submission-1

---
+++ Data Structures & Algorithms/search-insert-position/submission-1.py
@@ -0,0 +1,15 @@
+import bisect
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > target:
+                r -= 1
+            else:
+                l += 1
+    
+        return bisect.bisect_right(nums, target)
