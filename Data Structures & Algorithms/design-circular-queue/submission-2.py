18d5aad7ab1655896768247628cdb9580c28d28a
molochnicha2000-hue <molochnicha2000@gmail.com>
2026-06-12 20:09:33 +1000

    Add: design-circular-queue - submission-2

---
+++ Data Structures & Algorithms/design-circular-queue/submission-2.py
@@ -0,0 +1,47 @@
+class MyCircularQueue:
+    def __init__(self, k: int):
+        self.queue = []
+        self.k = k
+
+    def enQueue(self, value: int) -> bool:
+        #print(self.queue)
+        if len(self.queue) == self.k:
+            return False
+        self.queue.append(value)
+        return True
+
+    def deQueue(self) -> bool:
+        if not self.queue:
+            return False
+        self.queue.pop(0)
+        return True
+
+    def Front(self) -> int:
+        if self.queue:
+            return self.queue[0]
+        return -1
+
+    def Rear(self) -> int:
+        if self.queue:
+            return self.queue[-1]
+        return -1
+
+    def isEmpty(self) -> bool:
+        if not self.queue:
+            return True
+        return False
+
+    def isFull(self) -> bool:
+        if len(self.queue) == self.k:
+            return True
+        return False
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
