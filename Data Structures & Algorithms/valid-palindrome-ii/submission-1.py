d3fd3a41c7c5397597ec4b090c6c6ac792e3b656
molochnicha2000-hue <molochnicha2000@gmail.com>
2026-06-19 17:12:32 +1000

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        k = 0
+        l, r = 0, len(s) - 1
+
+        def dfs(l, r):
+            while l <= r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+
+        while l < r:
+            if s[l] != s[r]:
+                return dfs(l + 1, r) or dfs(l, r - 1)
+            l += 1
+            r -= 1
+        return True
+
