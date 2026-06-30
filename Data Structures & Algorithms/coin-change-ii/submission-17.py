06a87b6c10eb286cf17c741a7224cd23818a25c5
molochnicha2000-hue <molochnicha2000@gmail.com>
2026-06-30 12:37:30 +1000

    Add: coin-change-ii - submission-17

---
+++ Data Structures & Algorithms/coin-change-ii/submission-17.py
@@ -0,0 +1,19 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        N = len(coins)
+        coins.sort()
+        dp = [[0] * (N + 1) for _ in range(amount + 1)]
+
+        for r in range(N + 1):
+            dp[0][r] = 1
+          
+        for a in range(amount + 1):
+
+            for i in range(N - 1, -1, -1):
+                coin = coins[i]
+                if a - coin >= 0:
+                    comb = dp[a][i + 1]
+                    comb += dp[a - coin][i]
+                    dp[a][i] = comb
+        
+        return dp[amount][0]
