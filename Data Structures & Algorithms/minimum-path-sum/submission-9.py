4e1c41637cd5f423edba2e61250c580b56c863d4
molochnicha2000-hue <molochnicha2000@gmail.com>
2026-08-19 10:26:08 +1000

    Add: minimum-path-sum - submission-9

---
+++ Data Structures & Algorithms/minimum-path-sum/submission-9.py
@@ -0,0 +1,20 @@
+from functools import cache
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        d = [(1, 0), (0, 1)]
+
+        @cache
+        def dfs(r, c):
+            if r == rows - 1 and c == cols - 1:
+                return grid[r][c]
+            
+            best = float('inf')
+            for dr, dc in d:
+                nr, nc = dr + r, dc + c
+                if 0 <= nr < rows and 0 <= nc < cols:
+                    current = grid[r][c] + dfs(nr, nc)
+                    best = min(best, current)
+            return best
+        return dfs(0, 0)
+        
